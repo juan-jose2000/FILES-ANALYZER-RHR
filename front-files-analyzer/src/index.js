1ec8ebdee0942d07329c49bbbe6963c6c5627ba2
juan-jose2000 <juan.palma@beesoftware.mx>
2021-10-24 19:48:03 -0500

    no puedo combinar la ramas, ptm

---
+++ front-files-analyzer/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Login } from './login/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.css/main.css';
+
+
+
+
+
+
+
+ReactDOM.render(<Login/>, document.getElementById('root'));
---
+++ front-files-analyzer/src/login/Login.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from '../img/img-login.png';
+// import './login.css';
+
+export const Login = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log("hola");
+    }
+
+    return (
+        <section className="main-section-wrapper">
+            <div className="content-wrapper login">
+                <div className="content">
+                    <img src={logo} className="img-login" />
+
+                    <h1 className="titulo text-uppercase">Accesar <br/>al <b>sistema</b> </h1>
+
+                    <form onSubmit={handleLogin} className="login-form">
+                        <input type="text" className="input-form" name="username" id="username" placeholder="Usuario"/>
+                        <input type="text" className="input-form" name="password" id="password" placeholder="Contraseña"/>
+
+                        <button type="submit" className="button mt-3">Ingresar</button>
+                    </form>
+                    <button type="button" className="btn btn-link mt-3">Olvide mi contraseña</button>
+
+                </div>
+            </div>
+        </section>
+    )
+}

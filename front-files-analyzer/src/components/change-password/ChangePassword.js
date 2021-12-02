0c0e8c8681891480008198f7a931843b02c9b6eb
juan-jose2000 <juan.palma@beesoftware.mx>
2021-12-02 02:48:52 -0600

    actualizar contrasena funcionando

--- front-files-analyzer/src/components/change-password/ChangePassword.js
+++ front-files-analyzer/src/components/change-password/ChangePassword.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from "../hooks/useForm/useForm";
 import logo from '../../img/img-login.png';
 import _ from "lodash";
+import { toast } from 'react-toastify';
+import { updateUser } from '../../redux/actions/userListAction';
 
 const ChangePassword = () => {
 
+    const dispatch = useDispatch();
+    const user = useSelector( state => state.auth.user );
+
+    const { _id } = user;
+
     const [ formValues, handleInputChange ] = useForm({
-        currentPassword:"",
         newPassword:"",
         repeatPassword:"",
     });
@@ -16,13 +23,16 @@ const ChangePassword = () => {
         textError: ""
     });
 
-    const { currentPassword, newPassword, repeatPassword } = formValues;
+    const { newPassword, repeatPassword } = formValues;
     const { mostrarError, textError } = error; 
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(_.isEmpty(currentPassword) || _.isEmpty(newPassword) || _.isEmpty(repeatPassword)  ) {
+        const data = { _id, password: newPassword, }
+        console.log(data);
+
+        if(_.isEmpty(newPassword) || _.isEmpty(repeatPassword)  ) {
             setError({
                 mostrarError: true,
                 textError: "Rellene todos los campos",
@@ -33,14 +43,23 @@ const ChangePassword = () => {
                     mostrarError: true,
                     textError: "Las contraseñas no coinciden",
                 });
+                return  toast.error('Las contraseñas no coinciden', {
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
             } else {
                 setError({
                     mostrarError: false,
                 });
+                dispatch(updateUser(data));
             }
         }
-        
-        console.log(formValues);
     }
 
 
@@ -58,11 +77,18 @@ const ChangePassword = () => {
                 }
 
                 <form onSubmit={ handleSubmit }  className="login-form">
-                    <input type="password" name="currentPassword" value={ currentPassword } onChange={handleInputChange} className="input-form" placeholder="Contraseña actual" />
-                    <input type="password" name="newPassword" value={ newPassword } onChange={handleInputChange} className="input-form" placeholder="Nueva contraseña" />
-                    <input type="password" name="repeatPassword" value={ repeatPassword } onChange={handleInputChange} className="input-form" placeholder="Repetir contraseña" />
+                    <input type="password" name="newPassword" value={ newPassword } onChange={handleInputChange} className="input-form" placeholder="Nueva contraseña" 
+                        pattern="^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$"
+                        title="La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. Puede tener otros símbolos."
+                        required
+                    />
+                    <input type="password" name="repeatPassword" value={ repeatPassword } onChange={handleInputChange} className="input-form" placeholder="Repetir contraseña" 
+                        pattern="^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$"
+                        title="La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. Puede tener otros símbolos."
+                        required
+                    />
 
-                    <button type="submit" className="button mt-3">Enviar un email</button>
+                    <button type="submit" className="button mt-3">Actualizar contraseña</button>
                 </form>
             </div>
         </div>
